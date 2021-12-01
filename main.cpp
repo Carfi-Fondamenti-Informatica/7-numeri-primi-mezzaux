a7b45db1569fc725a9af7291ee42abbc1f257e1b
Alessandro Mezzacasa <91669008+mezzaux@users.noreply.github.com>
2021-12-01 19:04:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include "lib.h"
-using namespace std;
 
-int main(){
-  int a=0;
-  cin>>a;
-  bool ris=numeriprimi(a);
-  if(ris){
-    cout<<"numero primo"<<endl;
-  }else{
-    cout<<"numero non primo"<<endl;
-  }
-return 0;
+int main() {
+    using namespace std;
+    int a=0, b=2;
+    cin>>a;
+    bool ris= numeriprimi(a,b);
+    if(ris){
+        cout<<"numero primo"<<endl;
+    }else{
+        cout<<"numero non primo"<<endl;
+    }
+    return 0;
 }
