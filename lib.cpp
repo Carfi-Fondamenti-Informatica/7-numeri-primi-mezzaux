909d0e6b67475b8867b73243a8479a20f33cb77b
Alessandro Mezzacasa <91669008+mezzaux@users.noreply.github.com>
2021-12-01 19:05:09 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,29 +1,20 @@
 #include "lib.h"
-bool numeriprimi(int n){
-  if(n>2){
-    int resto=0, d=2;
-    resto=n%d;
-    if(resto==0){
-      return false;
+bool numeriprimi(int n, int d){
+    int rest;
+    if(n>1){
+        if(d<n){
+            rest=n%d;
+            if(rest !=0){
+                return numeriprimi(n,d+1);
+            }else{
+                return false;
+            }
+        }else{
+            return true;
+        }
+    }else if(n==2){
+        return true;
     }else{
-      return numeriprimi(n,d+1);
+        return false;
     }
-  }else if(n==2){
-    return true;
-  }else {
-    return false;
-  }
-}
-bool numeriprimi(int n;int d){
-  int rest=0;
-  if(d<n){
-    rest=n%d;
-    if(rest!=0){
-      return numeriprimi(n,d+1);
-    }else{
-      return false;
-    }
-  }else{
-    return true;
-  }
 }
